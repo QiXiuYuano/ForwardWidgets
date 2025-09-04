7c3ad50f38c8f6ad161263e271a1f4e2110c8b6e
七修远 <wchge63@gmail.com>
2025-09-04 16:22:53 +0800

    Update misaka_danmu_debug.js

--- widgets/misaka_danmu_debug.js
+++ widgets/misaka_danmu_debug.js
@@ -21,7 +21,7 @@
 WidgetMetadata = {
     id: "misaka.danmaku.debug",
     title: "Misaka DEBUG",
-    version: "1.0.0",
+    version: "0.0.2",
     requiredVersion: "0.0.2",
     description: "Misaka弹幕服务模块DEBUG",
     author: "QiXiuYuano",
